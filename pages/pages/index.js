82f37070ac3b3c11a24396a17c8fdeac978d8219
Tdrizzle1021 <dollardtristan21@gmail.com>
2026-01-29 09:22:24 -0700

    Delete pages directory

--- pages/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40, fontFamily: "Arial, sans-serif" }}>
-      <h1>Find Your Size Across Brands</h1>
-      <p>This is a live user test preview.</p>
-    </div>
-  );
-}
